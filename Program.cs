18c9a4a38d2ef614005953ee796de8050780c237
Hema <hemasahu019@gmail.com>
2022-08-06 19:22:35 +0530

    Added Welcome page code to main

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedListUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Linked List Data Structure Problems using C# Generics"); //welcome message
+        }
+    }
+}
